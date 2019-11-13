4b59fe4ac41db90af96379312ece7a82170d860c
ABhinavRajput-theCreator <abhinav.bhardwaj_cs18@gla.ac.in>
2019-11-13 14:48:50 +0530

    Initial commit

---
+++ .idea/college.py
@@ -0,0 +1,9 @@
+class college:
+    def Btech(self,a=0):
+        if a==0:
+            print("non argumentive")
+        else:
+            print("argumentive")
+obj = college()
+obj.Btech()
+obj.Btech(10)
---
+++ functionoverloading.py
@@ -0,0 +1,7 @@
+class functionOverloading:
+    def Btech(self):
+        print("Btech from CS")
+    def Btech(self):
+        print("Btech from EC")
+obj =functionOverloading()
+obj.Btech()
